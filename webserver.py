e0a2f2b133e9f001f668962fb6e7ef3ac0adf9a2
shollercoaster <saumyachat03@gmail.com>
2023-03-15 10:04:59 +0530

    updated POST functionality and added restaurants page

--- webserver.py
+++ webserver.py
@@ -1,32 +1,87 @@
-import lib2to3
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import cgi
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind = engine)
+session = DBSession()
+
 class webserverHandler(BaseHTTPRequestHandler):
+    """docstring for webserverHandler"""
+
     def do_GET(self):
         try:
+            if self.path.endswith('/restaurants'):
+                self.send_response(200)
+                self.send_header('Content-Type', 'text/html')
+                self.end_headers()
+                output = "" 
+                output += "<html><body>"
+                for restaurant in session.query(Restaurant).all():
+                    output += "<h1> %s </h1>" % restaurant.name
+                output += "</html></body>"
+                self.wfile.write(output.encode())
+                print(output)
+                return
+
+
+        
             if self.path.endswith("/hello"):
                 self.send_response(200)
-                self.send_header('Content-type', 'text/html')
+                self.send_header('Content-Type', 'text/html')
                 self.end_headers()
 
                 output = ""
-                output += "<html><body>Hello!</body></html>"
-                self.wfile.write(output.encode("utf-8"))
+                output += '<html><body>Hello!'
+                output += '<form method="POST" enctype="multipart/form-data" action="/hello"><h2> What would you like me to say?</h2><input name="message" type="text" /><input type="submit" value="Submit" /></form>'
+                output += '</body></html>'
+                self.wfile.write(output.encode())
                 print(output)
                 return
-            
+
             if self.path.endswith("/hola"):
                 self.send_response(200)
-                self.send_header('Content-type', 'text/html')
+                self.send_header('Content-Type', 'text/html')
                 self.end_headers()
 
                 output = ""
-                output += "<html><body>&#16Hola <a href = '/hello' >Back to Hello</a></body></html>"
-                self.wfile.write(output.encode("utf-8"))
+                output += '<html><body>&#161Hola <a href="/hello">Back to Hello</a>'
+                output += '<form method="POST" enctype="multipart/form-data" action="/hello"><h2> What would you like me to say?</h2><input name="message" type="text" /><input type="submit" value="Submit" /></form>'
+                output += '</body></html>'
+                self.wfile.write(output.encode())
                 print(output)
                 return
-            
+
         except IOError:
-            self.send_error(404, "File Not Found %s" % self.path)
+            self.send_error(404, "File not found %s" % self.path)
+
+    def do_POST(self):
+        try:
+            self.send_response(301)
+            self.send_header('Content-Type', 'text/html')
+            self.end_headers()
+            ctype, pdict = cgi.parse_header(self.headers.get('Content-Type'))
+            content_len = int(self.headers.get('Content-length'))
+            pdict['boundary'] = bytes(pdict['boundary'], "utf-8")
+            pdict['CONTENT-LENGTH'] = content_len
+            if ctype == 'multipart/form-data':
+                fields = cgi.parse_multipart(self.rfile, pdict)
+                message_content = fields.get('message')
+            output = ''
+            output += '<html><body>'
+            output += '<h2> Okay, How about this: </h2>'
+            output += '<h1> %s </h1>' % message_content[0]
+            output += '<form method="POST" enctype="multipart/form-data" action="/hello"><h2> What would you like me to say?</h2><input name="message" type="text" /><input type="submit" value="Submit" /></form>'
+            output += '</body></html>'
+            self.wfile.write(output.encode())
+            print(output)
+        except:
+            self.send_error(404, "{}".format(sys.exc_info()[0]))
+            print(sys.exc_info())
+
 
 def main():
     try:
@@ -34,9 +89,11 @@ def main():
         server = HTTPServer(('', port), webserverHandler)
         print("Web server running on port %s" % port)
         server.serve_forever()
+
     except KeyboardInterrupt:
-        print("^C entered, stopping web server...")
+        print(" ^C entered stopping web server...")
         server.socket.close()
+
 
 if __name__ == '__main__':
     main()
